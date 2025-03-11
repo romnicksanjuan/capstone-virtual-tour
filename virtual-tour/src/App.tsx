f6d00c71a2b4a0eed829a02de216ba974eb87183
romnick <romnicksanjuan9@gmail.com>
2025-03-11 19:49:10 +0800

    a

--- virtual-tour/src/App.tsx
+++ virtual-tour/src/App.tsx
@@ -2,13 +2,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import './App.css'
 import Home from './components/Home'
-// import Academic from './components/Academic'
-// import About from './components/About'
-// import Achievements from './components/Achievements'
-// import Contact from './components/Contact'
-// import Scholarship from './components/Scholarship'
-// import Test from './components/Test'
-// import Rules from './components/Rules'
+import Academic from './components/Academic'
+import About from './components/About'
+import Achievements from './components/Achievements'
+import Contact from './components/Contact'
+import Scholarship from './components/Scholarship'
+import Test from './components/Test'
+import Rules from './components/Rules'
 
 function App() {
 
@@ -18,13 +18,13 @@ function App() {
         <Routes>
 
           <Route path='/' element={<Home />} />
-          {/* <Route path='/academics' element={<Academic />} />
+          <Route path='/academics' element={<Academic />} />
           <Route path='/achievements' element={<Achievements />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/scholarships' element={<Scholarship />} />
           <Route path='/test' element={<Test />} />
-          <Route path='/rules' element={<Rules />} /> */}
+          <Route path='/rules' element={<Rules />} />
         </Routes>
       </BrowserRouter>
     </>
